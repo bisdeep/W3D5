2884753bb33805ea5f46ff7ca50d4c7c42ff3602
bisdeep <deepbiswasrepo@gmail.com>
2021-07-02 15:39:38 -0400

    dfs - debugging

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -6,10 +6,37 @@ class PolyTreeNode
         @parent = nil 
         @children = [] 
     end
+
     def parent=(new_parent)
         parent.children.delete(self) if parent != nil 
         @parent = new_parent
         parent.children << self if new_parent != nil 
     end
+
+    def add_child(node)
+        node.parent = self
+    end
+
+    def remove_child(node)
+        raise "Error" if node.parent.children.none? {|child| child == node }
+        
+        node.parent = nil
+        
+    end
+
+    def dfs(value)
+        return self if value == self.value
+
+        self.children.each do |child|
+            results = child.dfs(value)
+            return child if (results != nil) && (results.value == value)
+        end
+        nil
+
+    end
     
 end
+    #a
+  # b c
+ # c d e
+# f g h i
---
+++ skeleton/lib/searchable.rb
@@ -0,0 +1,11 @@
+require relative "00_tree_node.rb"
+
+class Searchable
+
+    def dfs(value)
+        return value if value == PolyTreeNode.value
+
+    end
+
+
+end
